ede4d6c2a59be7496c374c8c742cf7dc1558a0e0
Okamiz <jojo.berkes@gmail.com>
2023-06-01 15:19:43 +0200

    Added Creative Mode Tab

---
+++ src/main/java/net/okamiz/steamworkssymphonymod/item/ModCreativeModeTabs.java
@@ -0,0 +1,22 @@
+package net.okamiz.steamworkssymphonymod.item;
+
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.event.CreativeModeTabEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.okamiz.steamworkssymphonymod.SteamworksSymphonyMod;
+
+@Mod.EventBusSubscriber(modid = SteamworksSymphonyMod.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class ModCreativeModeTabs {
+    public static CreativeModeTab STEAMWORKS_SYMPHONY_TAB;
+
+    @SubscribeEvent
+    public static void registerCreativeModeTabs(CreativeModeTabEvent.Register event){
+        STEAMWORKS_SYMPHONY_TAB = event.registerCreativeModeTab(new ResourceLocation(SteamworksSymphonyMod.MOD_ID, "steamworks_symphony_tab"),
+                builder -> builder.icon(() -> new ItemStack(ModItems.IRON_PLATE.get()))
+                        .title(Component.translatable("creativemodetab.steamworks_symphony_tab")));
+    }
+}
