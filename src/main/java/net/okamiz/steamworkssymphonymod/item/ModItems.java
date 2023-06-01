46d2fb1070d55bcce59fdc226872668b99098eda
Okamiz <jojo.berkes@gmail.com>
2023-06-01 14:42:11 +0200

    Created ModItems Class

---
+++ src/main/java/net/okamiz/steamworkssymphonymod/item/ModItems.java
@@ -0,0 +1,14 @@
+package net.okamiz.steamworkssymphonymod.item;
+
+import net.minecraft.world.item.Item;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.okamiz.steamworkssymphonymod.SteamworksSymphonyMod;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, SteamworksSymphonyMod.MOD_ID);
+
+
+
+}
